4699c9352f688b8746dae91f1177b926bd9af901
Malte Simmat <malte.simmat@alho-group.com>
2024-09-05 11:30:49 +0200

    Erstellen der Oberfläche

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace LF10_Lager_Projekt
 {
@@ -20,14 +21,37 @@ namespace LF10_Lager_Projekt
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Popup popup = new Popup();
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void hinzufuegenButton_Click(object sender, RoutedEventArgs e)
         {
+            openPopup();
+        }
 
+        private void bearbeitenButton_Click(object sender, RoutedEventArgs e)
+        {
+            openPopup();
+        }
+
+        private void loeschenButton_Click(object sender, RoutedEventArgs e)
+        {
+            openPopup();
+        }
+
+        public void openPopup()
+        {
+            popup.Show();
+            popup.Activate();
+        }
+
+        public void OnWindowClosing(object sender, CancelEventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
