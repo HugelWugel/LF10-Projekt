8ec830d5180b427959b892020d077a60695667d0
Malte Simmat <malte.simmat@alho-group.com>
2024-09-05 09:06:39 +0200

    .

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace LF10_Lager_Projekt
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
